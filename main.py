a0117b6429855e5263c82a90d6ff8a0f5a4a3cef
Oute-Oute <thomas.blumstein@live.fr>
2026-07-10 13:14:22 +0200

    update sleep

--- main.py
+++ main.py
@@ -85,7 +85,8 @@ class stream(threading.Thread):
                         stream=True
 
                     cache['page'] = {"type":f"{"videostream" if stream else "web"}", "url":f"{f"rtmp://{SERVER_ADRESS}/{"zas" if zas else "ads/Pubs-cspace"}" if stream else "https://www.planete-sciences.org/espace/scae/qualifications&contest=cspace" }"}
-                    time.sleep(5)
+                    time.sleep(1)
+                    print('maj')
             except (KeyboardInterrupt):
                 sys.exit()
             except (EOFError):
