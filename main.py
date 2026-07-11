2bdbda4c70250eb74c38655f8aea81c434038ad8
Oute-Oute <thomas.blumstein@live.fr>
2026-07-11 17:56:15 +0200

    try resize

--- main.py
+++ main.py
@@ -20,8 +20,12 @@ default_page = {"type":"status", "text":"RPiCaster display offline."}
 cache = {"page": default_page}
 
 # VLC_COMMANDLINE = "cvlc --loop --fullscreen --no-osd --aout=pulse --network-caching=10000"
-VLC_COMMANDLINE = "cvlc --loop --aout=alsa --alsa-audio-device=default:CARD=vc4hdmi --fullscreen --no-osd --network-caching=10000 --file-caching=5000"
-VLC_COMMANDLINE = "cvlc --loop --aout=alsa --alsa-audio-device=plughw:CARD=vc4hdmi,DEV=0 --no-audio-time-stretch --fullscreen --no-osd"
+#VLC_COMMANDLINE = "cvlc --loop --aout=alsa --alsa-audio-device=default:CARD=vc4hdmi --fullscreen --no-osd --network-caching=10000 --file-caching=5000"
+print(sys.argv)
+if "--fake-fullscreen-hd" in sys.argv:
+    VLC_COMMANDLINE = "cvlc --loop --aout=alsa --alsa-audio-device=plughw:CARD=vc4hdmi,DEV=0 --no-audio-time-stretch --fullscreen --no-osd"
+else :
+    VLC_COMMANDLINE = "cvlc --loop --aout=alsa --alsa-audio-device=plughw:CARD=vc4hdmi,DEV=0 --no-audio-time-stretch --fullscreen --video-filter=scale --scale=0.6667 --no-osd"
 SERVER_ADRESS='83.228.216.138'
 
 def main():
@@ -118,6 +122,8 @@ class MainWindow(QMainWindow):
         self.label_message.setWordWrap(True)
 
         self.browser = QWebEngineView()
+        if "--fake-fullscreen-hd" in sys.argv:
+            self.browser.resize(1280,720)
         self.browser_img = QWebEngineView()
 
         self.black = QWidget()
